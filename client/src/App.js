6dc748fbb9fce4d6638d6c5e6b6af90f2f2ac2e4
Geek Boy <github@geekboy.com.au>
2021-09-20 19:55:59 +1000

    Updated client to make request to one recipe /api/recipes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+// Import the essential components from react-native
+// import {
+//   StyleSheet, Button, View, SafeAreaView,
+//   Text, Alert
+// } from 'react-native';
+
+// import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+
+// import "@fontsource/roboto"; 
 import logo from './logo.svg';
 import './App.css';
+import Webpages from './webpages';
+import MainMenu from './Components/main_menu';
 
 class App extends Component {
 state = {
@@ -9,11 +22,15 @@ state = {
 
   componentDidMount() {
     this.callBackendAPI()
-      .then(res => this.setState({ data: res.express }))
+      .then(res => {
+        // console.log(res.json())
+        this.setState({ data: res.express })
+      })
       .catch(err => console.log(err));
   }
     // fetching the GET route from the Express server which matches the GET route from server.js
   callBackendAPI = async () => {
+    // const response = await fetch('/api/menus');
     const response = await fetch('/hello');
     const body = await response.json();
 
@@ -23,15 +40,60 @@ state = {
     return body;
   };
 
+  callAPIMenu = async () => {
+    const response = await fetch('/api/menus');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message) 
+    }
+    return body;
+  };
+  
   render() {
     return (
+    <Fragment>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Contenta</h1>
         </header>
+        <MainMenu />
+        <div>
+          <Webpages />
+        </div>
+        <div className="App-body">
+          <ul>
+            <li><a href="/api/recipes">Recipes</a></li>
+            <li><a href="/healthcheck">Healthcheck</a></li>
+            <li><a href="/hello">Hello</a></li>
+          </ul>
+
+          {/* <Button onClick={() => alert('Clicked')} variant="contained" color="primary">
+            Hello World
+          </Button> */}
+        <Button variant="contained" color="primary" onClick={
+          () => {
+            this.callAPIMenu()
+            .then(res => {
+              var today = new Date();
+              var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+              var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+              var dateTime = date+'_'+time;
+            
+              this.setState({ data:'Menu id 0 ' + res.data[0]['id'] + ' at ' + dateTime })
+            })
+            .catch(err => console.log(err));
+            }
+          }
+          >Get Menus</Button>
+        </div>
+      </div>
+      <div className="App-footer">
         <p className="App-intro">{this.state.data}</p>
       </div>
+      </Fragment>
+
     );
   }
 }

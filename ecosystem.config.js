9ee5e68b068fd199da3bbbb2e6f1c0f89a3ea7c0
Geek Boy <dev@geekboy.com.au>
2021-10-04 10:51:24 +1000

    Removed second doorbell app from pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'contentajs',
+      name: 'doorbell',
       script: 'lib/server.js',
 
       // Options reference: https://pm2.io/docs/runtime/reference/ecosystem-file/
@@ -19,27 +19,7 @@ module.exports = {
       env_production: {
         NODE_ENV: 'production',
       },
-    },
-    {
-      name: 'rpi_doorbell',
-      script: 'bin/www',
-      cwd: '/home/markc/Projects/rpi_doorbell_github',
-      // Options reference: https://pm2.io/docs/runtime/reference/ecosystem-file/
-      port: 3002,
-      instances: '1',
-      autorestart: false,
-      watch: false,
-      max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'development',
-      },
-      env_staging: {
-        NODE_ENV: 'staging',
-      },
-      env_production: {
-        NODE_ENV: 'production',
-      },
-    },
+    }
   ],
 
   // Use pm2 to deploy your ContentaJS app.

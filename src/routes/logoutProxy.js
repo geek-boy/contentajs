49e12fa1578e1ae23e131f67fa5c3572e71e326b
Geek Boy <dev@geekboy.com.au>
2021-10-05 20:13:45 +1000

    working of /signout

--- src/routes/logoutProxy.js
+++ src/routes/logoutProxy.js
@@ -9,12 +9,12 @@ import type {
 const proxy = require('express-http-proxy');
 
 module.exports = (req: Request, res: Response, next: NextFunction): void => {
-  console.log("----- loginProxy --------")
-  // console.log(req)
+  console.log("----- logoutProxy --------")
+  console.log(req.body)
 
   return proxy(req.cmsHost,
     {
-      proxyReqPathResolver: req => `/user/login?_format=json`,
+      proxyReqPathResolver: req => `/user/logout?_format=json&token=` + req.body.logout_token + `&csrf_token=` + req.body.csrf_token,
     }
   )(req, res, next);
 };
